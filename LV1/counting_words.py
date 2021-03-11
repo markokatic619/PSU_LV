e0d9fb0942b8ed75c7a5b5a3ce8af75fe5d71701
Marko Katic <david.zovko@e-radionica.com>
2021-03-11 14:34:36 +0100

    dodan file

---
+++ LV1/counting_words.py
@@ -0,0 +1,20 @@
+# how many times each word appears in the txt file
+
+fname = raw_input('Enter the file name: ')  #e.g. www.py4inf.com/code/romeo.txt
+try:
+    fhand = open(fnamex)
+except:
+    print 'File cannot be opened:', fname
+    exit()
+
+counts = dict()
+for line in fhand:
+    words = line.split()
+    
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] = 1
+
+print counts
